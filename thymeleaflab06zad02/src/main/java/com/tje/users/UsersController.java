9b9a3e8a18eb97f5f69716f7e885cde4d9ff6a9e
Krzysztof Dąbrowski <dabraski.222@gmail.com>
2023-11-23 16:38:47 +0100

    lab06zad02

---
+++ thymeleaflab06zad02/src/main/java/com/tje/users/UsersController.java
@@ -0,0 +1,27 @@
+package com.tje.users;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class UsersController {
+
+    @GetMapping("/")
+    public String home(Model model) throws ParseException {
+        String startDateString = "20/05/2007 07:32";
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        long time = df.parse(startDateString).getTime();
+        long daysTillRegistration = (new Date().getTime() - time) / (1000 * 60 * 60 * 24);
+        Date startDate = df.parse(startDateString);
+        User user = new User(2, "Artur", 34, User.UserType.ADMIN, startDate);
+        model.addAttribute("user", user);
+        model.addAttribute("daysTillRegistration", daysTillRegistration);
+        return "home";
+    }
+}
