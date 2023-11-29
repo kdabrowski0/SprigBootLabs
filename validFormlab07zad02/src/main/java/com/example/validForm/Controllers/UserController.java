e3db78f48d05c4540534bd4d4439c8a88b327f8a
Javelux <dabraski.222@gmail.com>
2023-11-29 16:26:45 +0100

    lab07 zad02

---
+++ validFormlab07zad02/src/main/java/com/example/validForm/Controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.example.validForm.Controllers;
+
+import com.example.validForm.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+@Slf4j
+@Controller
+public class UserController {
+    @GetMapping("/createUser")
+    public String createUser(Model model) {
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/createUser")
+    public String registerUser(@Valid User user, Errors errors) {
+        if(errors.hasErrors()) {
+            return "form";
+        }
+        log.info("User created: " + user);
+        return "redirect:/";
+    }
+
+}
